839a98e2c8c1ee37d0e673576cbc24fafbd28d7f
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-29 18:16:22 -0300

    components hierarchy

---
+++ 2_instalacao/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Estou trabalhando no momento.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>Phyton</li>
+      <li>PHP</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Info'
+  }
+</script>
