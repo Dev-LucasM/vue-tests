fe7f8bcfb2b472ff624d94801b50b491408e0d66
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-29 16:55:18 -0300

    feat: starting course

---
+++ 2_instalacao/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <p>Olá {{ testando }}</p>
+  </div>
+</template>
+
+<script>
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      PrimeiroComponente
+    },
+    data() {
+      return {
+        testando:  'Teste'
+      }
+    },
+  }
+</script>
