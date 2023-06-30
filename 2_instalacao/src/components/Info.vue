5ae1f7460629408f2fa2a33ba803574f97be0250
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-06-30 10:37:53 -0300

    feat: knowing the directives

--- 2_instalacao/src/components/Info.vue
+++ 2_instalacao/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novos desafios</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>JavaScript</li>
       <li>Phyton</li>
       <li>PHP</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Info'
+    name: 'Info',
+    data() {
+      return {
+        esta_trabalhando: false,
+        mostrar_email: true,
+        email: "lucas@email.com"
+      }
+    }
   }
 </script>
